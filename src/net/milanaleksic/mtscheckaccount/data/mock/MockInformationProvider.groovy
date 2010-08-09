9b7851e7773ccfd96370f541a1666084a79d874c
Milan Aleksic <milanaleksic@gmail.com>
2010-08-09 10:36:05 +0000

    assertion added to mock code
    --HG--
    extra : convert_revision : svn%3A78f96c99-00cc-493c-a0b6-3717ecfe2a42%4067

--- src/net/milanaleksic/mtscheckaccount/data/mock/MockInformationProvider.groovy
+++ src/net/milanaleksic/mtscheckaccount/data/mock/MockInformationProvider.groovy
@@ -4,10 +4,22 @@ import net.milanaleksic.mtscheckaccount.data.*
 
 public class MockInformationProvider implements InformationProvider {
 
-  public def provideInformation(params, String port, Closure closure) {
-	closure 'Mozete ugasiti program'
-	
-	def testReceivedFromMts ="""
+    public def provideInformation(params, String port, Closure closure) {
+        closure 'Mozete ugasiti program'
+
+        assert """
++CUSD: 1,"mt:s vodic
+Stanje: nedostupno
+Izaberite opciju i potvrdite.
+
+1. Prepaid dopuna
+2. Stanje racuna
+3. Dodatne usluge
+4. Zabava i info
+",15
+""" =~ params.data.pre[2].@response
+
+        def testReceivedFromMts = """
 +CUSD: 0,"Tarifni profil: Profesionalna tarifa II
 Stanje: 125.56
 Preostali besplatni saobracaj:
@@ -16,7 +28,7 @@ Minuti van mts: 215
 SMS: 99987
 Podaci(kB): 3336380",15
 """
-	
-    closure(new MTSExtract().extract(testReceivedFromMts))
-  }
+
+        closure(new MTSExtract().extract(testReceivedFromMts))
+    }
 }
