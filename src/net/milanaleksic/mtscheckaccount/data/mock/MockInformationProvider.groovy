cdfbada7fa4b9f619afd0f77e0c5493f295c09ae
Milan Aleksic <milanaleksic@gmail.com>
2010-08-09 21:34:42 +0000

    mt:s again changed the format of the output of their service
    --HG--
    extra : convert_revision : svn%3A78f96c99-00cc-493c-a0b6-3717ecfe2a42%4083

--- src/net/milanaleksic/mtscheckaccount/data/mock/MockInformationProvider.groovy
+++ src/net/milanaleksic/mtscheckaccount/data/mock/MockInformationProvider.groovy
@@ -20,13 +20,10 @@ Izaberite opciju i potvrdite.
 """ =~ params.data.pre[2].@response
 
         def testReceivedFromMts = """
-+CUSD: 0,"Tarifni profil: Profesionalna tarifa II
-Stanje: 125.56
-Preostali besplatni saobracaj:
-Minuti u mts: 156
-Minuti van mts: 215
-SMS: 99987
-Podaci(kB): 3336380",15
++CUSD: 1,"Tarifni profil: Surf Classic
+Stanje: 0.00
+Preostali besplatan saobracaj:
+Podaci (kb):11175740",15
 """
 
         closure(new MTSExtract().extract(testReceivedFromMts))
