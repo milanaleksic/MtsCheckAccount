2f0003e298084030d390414050d4ca1e518c546d
milanaleksic <none@none>
2010-08-21 12:16:43 +0000

    Linux support for HUAWEI E1550, startup dir prepared for Linux dev also (32bit Serial.so copied), Idea Module file included and some typos
    --HG--
    extra : convert_revision : svn%3A78f96c99-00cc-493c-a0b6-3717ecfe2a42%40116

--- src/net/milanaleksic/mtscheckaccount/os/linux/LinuxLocator.groovy
+++ src/net/milanaleksic/mtscheckaccount/os/linux/LinuxLocator.groovy
@@ -2,7 +2,6 @@ package net.milanaleksic.mtscheckaccount.os.linux
 
 import groovy.util.slurpersupport.GPathResult
 import net.milanaleksic.mtscheckaccount.os.Locator
-import org.apache.commons.logging.*
 
 public class LinuxLocator extends Locator {
 
@@ -10,27 +9,48 @@ public class LinuxLocator extends Locator {
         super(config)
     }
 
-    //TODO: uzmi u obzir konfiguraciju (ime uredjaja za HAL pretragu)
     @Override protected String[] doGetModemLocationForDevice(GPathResult device) {
-        def found = 'hal-find-by-capability --capability modem'.execute().text.trim()
+        return doGetModemLocationForDeviceId(
+                device.linux.@port.text(),
+                device.linux.@udi.text(),
+                device.linux.@deviceId.text()
+        )
+    }
 
-        log.debug "Pronadjeni modem uredjaji:\n ${found}"
-
-        log.debug "\nAnaliza uredjaja:"
-        def udiForZTE = null
-        found.eachLine() {
-            def productId = "hal-get-property --udi ${it} --key info.product".execute().text.trim()
-            log.debug "udi=[${it}] => productId=[${productId}]"
-            if (productId.trim() == device.linux.@deviceId.text()) {
-                udiForZTE = it
-            }
+    private String[] doGetModemLocationForDeviceId(String port, String udi, String deviceId) {
+        if (port) {
+            log.debug "Koristim eksplicitno postavljen port [$port]"
+            return [port]
         }
-        if (udiForZTE == null)
-            throw new IllegalStateException('Uredjaj nije pronajen putem HAL pretrage')
-        log.debug "\nPronadjen je ZTE modem na identifikatoru: udi=[${udiForZTE}]"
-        def device_file = "hal-get-property --udi ${udiForZTE} --key linux.device_file".execute().text.trim()
-        log.debug "Konacno, lokacija modema je [${device_file}]"
-        return device_file
+
+        def udiForModem = null
+        if (udi) {
+            log.debug "Koristim eksplicitno postavljen UDI [$udi]"
+            udiForModem = udi
+        } else {
+            def found = 'hal-find-by-capability --capability modem'.execute().text.trim()
+
+            log.debug "Pronadjeni modem uredjaji:\n ${found}"
+
+            log.debug "\nAnaliza uredjaja:"
+
+            udiForModem = null
+            found.eachLine() {
+                def productId = "hal-get-property --udi ${it} --key info.product".execute().text.trim()
+                log.debug "udi=[${it}] => productId=[${productId}]"
+                if (productId.trim() == deviceId) {
+                    udiForModem = it
+                }
+            }
+
+            if (udiForModem == null)
+                return null
+            log.debug "\nPronadjen je modem na identifikatoru: udi=[${udiForModem}]"
+        }
+
+        def device_file = "hal-get-property --udi ${udiForModem} --key linux.device_file".execute().text.trim()
+        log.debug "Lokacija modema je [${device_file}]"
+        return [device_file]
     }
 
 }
