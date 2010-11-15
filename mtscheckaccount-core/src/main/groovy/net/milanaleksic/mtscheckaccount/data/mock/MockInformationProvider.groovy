e2d71a61c048a0d0cb10c8e6ee99cfb903d09383
Milan Aleksic <milanaleksic@gmail.com>
2010-11-15 21:08:33 +0100

    fix for change in text of information: now it's kB instead of kb

--- mtscheckaccount-core/src/main/groovy/net/milanaleksic/mtscheckaccount/data/mock/MockInformationProvider.groovy
+++ mtscheckaccount-core/src/main/groovy/net/milanaleksic/mtscheckaccount/data/mock/MockInformationProvider.groovy
@@ -12,18 +12,6 @@ public class MockInformationProvider extends InformationProvider {
     public def provideInformation(Closure closure) {
         closure 'Mozete ugasiti program'
 
-        assert """
-+CUSD: 1,"mt:s vodic
-Stanje: nedostupno
-Izaberite opciju i potvrdite.
-
-1. Prepaid dopuna
-2. Stanje racuna
-3. Dodatne usluge
-4. Zabava i info
-",15
-""" =~ params.data.pre[2].@response
-
         def testReceivedFromMts = """
 +CUSD: 1,"Tarifni profil: Surf Classic
 Stanje: 0.00
@@ -31,6 +19,6 @@ Preostali besplatan saobracaj:
 Podaci (kb):11175740",15
 """
 
-        closure(new MTSExtract().extract(testReceivedFromMts))
+        closure(MTSExtract.instance.extract(testReceivedFromMts))
     }
 }
