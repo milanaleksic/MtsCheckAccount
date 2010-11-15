3eaab1a61ca0c59c7191f297dc17ebcc91e33d99
Milan Aleksic <milanaleksic@gmail.com>
2010-11-15 23:53:22 +0100

    improved test content

--- mtscheckaccount-core/src/test/groovy/net/milanaleksic/mtscheckaccount/data/MTSExtractTest.groovy
+++ mtscheckaccount-core/src/test/groovy/net/milanaleksic/mtscheckaccount/data/MTSExtractTest.groovy
@@ -31,18 +31,18 @@ Podaci(kB): 3145728",15
         assertTrue("Polje Gprs nije dobro - ${bean.Gprs}", bean.Gprs == "3145728")
     }
 
-    void test_parser_current_old_string() {
+    void test_parser_current_surf_classic_string() {
         def bean = testSubject.extract('''
-+CUSD: 0,"Tarifni profil: Control_Mobilni_Net_3
-Stanje: 0.00
++CUSD: 0,"Tarifni profil: Surf Classic
+Stanje: 3.12
 Preostali besplatni saobracaj:
-Podaci (kb): 3145728",15
+Podaci (kb): 12214240",15
 ''')
-        assertTrue("Polje Stanje nije dobro - ${bean.Stanje}", bean.Stanje == "0.00")
+        assertTrue("Polje Stanje nije dobro - ${bean.Stanje}", bean.Stanje == "3.12")
         assertTrue("Polje UMrezi nije dobro - ${bean.UMrezi}", bean.UMrezi == "NEDOSTUPNO")
         assertTrue("Polje VanMreze nije dobro - ${bean.VanMreze}", bean.VanMreze == "NEDOSTUPNO")
         assertTrue("Polje Sms nije dobro - ${bean.Sms}", bean.Sms == "NEDOSTUPNO")
-        assertTrue("Polje Gprs nije dobro - ${bean.Gprs}", bean.Gprs == "3145728")
+        assertTrue("Polje Gprs nije dobro - ${bean.Gprs}", bean.Gprs == "12214240")
     }
 
     void test_parser_current_invalid_string() {
